2a0211722a10c3e5050d2dd24b11ad7d6bb52b2f
chenglei <123chenglei>
2019-01-08 11:36:39 +0800

    弹出toast的时候如果有正在弹出的，先取消之前的

--- lib/toast.dart
+++ lib/toast.dart
@@ -2,43 +2,85 @@ import 'package:flutter/material.dart';
 
 class Toast {
   static void showToast(BuildContext context, String message) {
-    OverlayState overlayState = Overlay.of(context);
-    OverlayEntry overlayEntry = OverlayEntry(builder: (context) {
+    _ToastView toastView = _ToastView();
+    if (toastView.showing) {
+      toastView.dismiss();
+    }
+    Future.delayed(Duration(milliseconds: 500))
+    .then((value) {
+      toastView.show(context, message);
+    });
+  }
+}
+
+
+class _ToastView {
+  bool showing = false;
+
+  _ToastView._construct() {}
+
+  factory _ToastView() => _getInstance();
+
+  static _ToastView _instance;
+
+  static _getInstance() {
+    if (_instance == null) {
+      _instance = _ToastView._construct();
+    }
+    return _instance;
+  }
+
+  OverlayState overlayState;
+  OverlayEntry overlayEntry;
+
+  show(BuildContext context, String message) {
+    overlayState = Overlay.of(context);
+    overlayEntry = OverlayEntry(builder: (context) {
       return Container(
           child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Container(
-              decoration: BoxDecoration(
-                color: Color.fromARGB(0x80, 0x00, 0x00, 0x00),
-                  borderRadius: BorderRadius.all(
-                      Radius.circular(4.0)
-                  )
-              ),
-              child: Container(
-                constraints: BoxConstraints(maxWidth: 201.0, minWidth: 132.0),
-                margin: EdgeInsets.all(16.0),
-                child: Align(
-                  child: Text(
-                    message,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        fontSize: 13.0,
-                        color: Colors.white,
-                        decoration: TextDecoration.none,
-                        fontWeight: FontWeight.normal),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                      color: Color.fromARGB(0x80, 0x00, 0x00, 0x00),
+                      borderRadius: BorderRadius.all(
+                          Radius.circular(4.0)
+                      )
                   ),
-                ),
-              ),
-            )
-          ],
-        ),
-      ));
+                  child: Container(
+                    constraints: BoxConstraints(maxWidth: 201.0, minWidth: 132.0),
+                    margin: EdgeInsets.all(16.0),
+                    child: Align(
+                      child: Text(
+                        message,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 13.0,
+                            color: Colors.white,
+                            decoration: TextDecoration.none,
+                            fontWeight: FontWeight.normal),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ));
     });
     overlayState.insert(overlayEntry);
+    showing = true;
     Future.delayed(Duration(seconds: 2)).then((value) {
-      overlayEntry.remove();
+      dismiss();
     });
+  }
+
+  dismiss() {
+    overlayEntry?.remove();
+    showing = false;
+  }
+
+  isShowing() {
+    return showing;
   }
 }

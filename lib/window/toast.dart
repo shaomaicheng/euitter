8fd5c574482d1d14a69400db1f725d02aaa45a5f
chenglei01 <chenglei01@corp.netease.com>
2020-06-25 23:23:57 +0800

    修改toast在iOS上的位置

--- lib/window/toast.dart
+++ lib/window/toast.dart
@@ -118,32 +118,36 @@ class _ToastState extends State<_Toast> with TickerProviderStateMixin {
       builder: (context, child) {
         return Opacity(
           opacity: animation.value,
-          child: Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Container(
-                  decoration: BoxDecoration(
-                      color: Color.fromARGB(0x80, 0x00, 0x00, 0x00),
-                      borderRadius: BorderRadius.all(Radius.circular(4.0))),
-                  child: Container(
-                    constraints:
-                        BoxConstraints(maxWidth: 201.0, minWidth: 132.0),
-                    margin: EdgeInsets.all(16.0),
-                    child: Align(
-                      child: Text(
-                        widget.message,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 13.0,
-                            color: Colors.white,
-                            decoration: TextDecoration.none,
-                            fontWeight: FontWeight.normal),
+          child: Padding(
+            padding: EdgeInsets.only(bottom: MediaQuery.of(context).size.height * 0.15),
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Color.fromARGB(0x80, 0x00, 0x00, 0x00),
+                        borderRadius: BorderRadius.all(Radius.circular(4.0))),
+                    child: Container(
+                      constraints:
+                      BoxConstraints(maxWidth: 201.0, minWidth: 132.0),
+                      margin: EdgeInsets.all(16.0),
+                      child: Align(
+                        child: Text(
+                          widget.message,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: 13.0,
+                              color: Colors.white,
+                              decoration: TextDecoration.none,
+                              fontWeight: FontWeight.normal),
+                        ),
                       ),
                     ),
-                  ),
-                )
-              ],
+                  )
+                ],
+              ),
             ),
           ),
         );
